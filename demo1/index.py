348e970dd5c3d004c0119306be997b699ed8ec8f
sky <hw_developer@163.com>
2019-11-18 09:10:03 +0800

    更新代码

--- demo1/index.py
+++ demo1/index.py
@@ -1,29 +1,34 @@
 import VoiceAi
 
 import turtle
-t=turtle.Turtle()
-t.shape("turtle")
-t.speed(1)
+#把海龟改为熊猫
+turtle.register_shape('panda.gif')
+turtle.shape('panda.gif')
+turtle.speed(1)
 
-turtleX=100
-turtleY=100
+turtleX=0
+turtleY=0
 
 def drawPlane(text):
   global turtleX
   global turtleY
 
   if(text.find('左')!=-1):
+       print("向左走")
        turtleX=turtleX-100
-       t.goto(turtleX,turtleY)
+       turtle.goto(turtleX,turtleY)
   elif (text.find('右')!=-1):
+       print("向右走")
        turtleX=turtleX+100
-       t.goto(turtleX,turtleY)
+       turtle.goto(turtleX,turtleY)
   elif (text.find('上')!=-1):
+       print("向上走")
        turtleY=turtleY+100
-       t.goto(turtleX,turtleY)
+       turtle.goto(turtleX,turtleY)
   elif (text.find('下')!=-1):
+       print("向下走")
        turtleY=turtleY-100
-       t.goto(turtleX,turtleY)
+       turtle.goto(turtleX,turtleY)
        
 
 
---
+++ 第1节课/02code/e3.py
@@ -0,0 +1,2 @@
+# 这是第一个单行注释
+print("hello python")
